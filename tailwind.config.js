ad6fbfe0e08e8ee42bce8683f4e4866254d02c87
ViniciusUrias <viniciusuriasdeabreu@gmail.com>
2021-05-24 12:42:45 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   purge: ["./public/**/*.html"],
   theme: {
-    extend: {},
+   
+    fontFamily: {
+      'sofia': ['Sofia Pro']
+    },
+    extend: {
+      backgroundImage: theme => ({
+        'home-bg': "url('/media/home-back.png')",
+        'home1-bg': "url('/media/home1.svg')",
+        'home-rocket': "url('/media/new-rocket.png')"
+      }),
+    },
   },
   variants: {},
   plugins: [],
